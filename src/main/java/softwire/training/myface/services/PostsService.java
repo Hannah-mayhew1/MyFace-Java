7d226197ea4bd5dd2e99f2fddba60359c32120bb
Hannah.Mayhew <hannah.mayhew@HannahMayhews-MacBook-Pro.local>
2019-04-04 11:27:11 +0100

    delete post

--- src/main/java/softwire/training/myface/services/PostsService.java
+++ src/main/java/softwire/training/myface/services/PostsService.java
@@ -5,27 +5,44 @@ import org.springframework.stereotype.Service;
 import softwire.training.myface.models.dbmodels.Post;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostsService extends DatabaseService {
 
     public List<Post> getPostsOnWall(String recipient) {
-        try (Handle handle = jdbi.open()) {
-            return handle
-                    .createQuery("SELECT * FROM posts WHERE recipient = :recipient")
-                    .bind("recipient", recipient)
+        return jdbi.withHandle(handle ->
+                handle.createQuery("SELECT * FROM posts WHERE recipient = :recipient")
+                        .bind("recipient", recipient)
+                        .mapToBean(Post.class)
+                        .list()
+        );
+    }
+
+    public Post getSinglePostOnWall(int id) {
+        return jdbi.withHandle(handle ->
+            handle.createQuery("SELECT * FROM posts WHERE id = :id")
+                    .bind("id", id)
                     .mapToBean(Post.class)
-                    .list();
-        }
+                    .findOnly()
+        );
     }
 
     public void createPost(String sender, String recipient, String content) {
-        jdbi.withHandle(handle ->
+        jdbi.useHandle(handle ->
                 handle.createUpdate("INSERT INTO posts (sender, recipient, content) VALUES (:sender, :recipient, :content)")
                         .bind("sender", sender)
                         .bind("recipient", recipient)
                         .bind("content", content)
                         .execute()
+        );
+    }
+
+    public void deletePost(int id) {
+        jdbi.useHandle(handle ->
+                handle.createUpdate("DELETE FROM posts WHERE id = :id")
+                    .bind("id", id)
+                    .execute()
         );
     }
 }
